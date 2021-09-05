b8d1f65bbe3e5fbb237a39d81324199f9e83f31d
Nem-Ajvaz <45509371+Nem-Ajvaz@users.noreply.github.com>
2021-09-05 15:59:40 +1000

    adding screen rendering of chat

--- controllers/websockets/messages.js
+++ controllers/websockets/messages.js
@@ -1,7 +1,7 @@
 function initMessagesSocket(socket, io) {
   socket.on('newMessage', msg => {
     console.log('message', msg);
-    io.emit('chatMessage', msg);
+    socket.broadcast.emit('message', msg); // Needs to be a broadcast so the user doesnt see their message twice.
   });
 }
 
--- utils/message-helper.js
+++ utils/message-helper.js
@@ -1,12 +1,12 @@
-const moment =require('moment'); 
+const moment = require('moment');
 
 let messageFormat = (username, text) => {
-    return {
-        username,
-        text,
-        time: moment().format('h:mm a')
-    };
+  return {
+    user_id, // either user from registration or the socket session
+    username, // from sign in
+    text, // message content from user
+    time: moment().format('h:mm a') // time format
+  };
 };
 
-module.exports = messageFormat;
-
+module.exports = { messageFormat };
